8b23f6d017946f3cc752ab30c427c31dbae33f3a
Danny Blaker <dzulzer@gmail.com>
2026-01-12 19:39:45 +1100

    Update GETTING_STARTED.md and spam_detection.py for clarity and path adjustments

--- use_cases/spam_detection.py
+++ use_cases/spam_detection.py
@@ -39,6 +39,9 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 warnings.filterwarnings('ignore')
 
 
